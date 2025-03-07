643c1bc06422cdffd6f54ec95a881b0c683ec6b4
Minhee-Jang/장민희 <minhee_jang@ewha.ac.kr>
2025-03-07 15:05:52 +0900

    [level 2] Title: 타겟 넘버, Time: 236.30 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,16 +1,14 @@
 def solution(numbers, target):
-    ans = 0
-    def dfs(index, sum_num, ans):
-    #재귀로 구현해보자 
-    # 만약에 끝까지 도달했으면 -> 된다는거? 
-        if index == len(numbers):
-            if sum_num == target:
-                ans +=1
-            index = 0
-            return ans # index 끝까지 확인했으면 돌아가기 
-        # 더하는 경우
-        # 빼는 경우
-        ans = dfs(index+1, sum_num + numbers[index], ans) + dfs(index+1, sum_num - numbers[index], ans)
-        return ans # 다음 스텝 구하기 
+    
+    test = [0]
+    s = []
+    sum_num = 0
+    for n in numbers:
+        s = []   # 그전까지는 신경 x 마지막 합들만 고려 -> 2**20개 
+        for _ in range(len(test)):
+            current = test.pop()
+            s.append(current + n)
+            s.append(current + n*(-1))
+        test = s      
 
-    return dfs(0, 0, ans)
+    return s.count(target)
