527485f964c34c03315d5259ecd48385e3aa629a
Minhee-Jang/장민희 <minhee_jang@ewha.ac.kr>
2025-03-18 14:42:40 +0900

    [level 3] Title: 베스트앨범, Time: 0.13 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -43,3 +43,21 @@ def solution(genres, plays):
                 ans.append(index_2)
             
     return ans
+
+
+def solution(genres, plays):
+    answer = []
+    
+    d = {e:[] for e in set(genres)}   # genre유니크 값만 기록
+    for e in zip(genres, plays, range(len(plays))):
+        d[e[0]].append((e[1], e[2]))  #각 장르에 play수랑 index
+    genreSort = sorted(list(d.keys()), key = lambda x : sum(map(lambda y : y[0], d[x])), reverse = True)
+    for g in genreSort:
+        temp = [e[1] for e in sorted(d[g], key = lambda x: (x[0], -x[1]), reverse = True)]
+        answer += temp[:min(len(temp), 2)]  # 2개 이하면 하나만 
+        
+    return answer
+    
+    
+    
+    
